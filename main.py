001f5b597fcb9f93c3c5b0354a69c5f486619dd4
IBSHAMI <78549117+IBSHAMI@users.noreply.github.com>
2021-11-27 17:34:17 +0800

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+# import beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+import re
+# import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import time
+import json
