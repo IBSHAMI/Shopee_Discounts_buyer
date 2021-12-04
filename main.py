ff488287c89f9e8743490d4b4b771ebe9da851c9
IBSHAMI <78549117+IBSHAMI@users.noreply.github.com>
2021-12-04 17:38:20 +0800

    feature: Added product name and rating info extraction using Selenium
    Used Selenium driver to extract the product name and rating given the product website link

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from product_reader import ProductReader
 # chrome driver path
 chrome_driver_path = r"C:\Users\user\PycharmProjects\Prof_Projects\chromedriver_win32\chromedriver.exe"
 service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service)
+
+# make chrome run in the background without opening a new window each time the program is run
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+# create the driver
+driver = webdriver.Chrome(service=service, options=options)
+
+# create product reader class
+product_page = ProductReader(url="https://shopee.com.my/PUMA-NU-TILITY-Men's-Tee-Basics-i.341487315.12327703115", driver=driver)
+product_name, product_rating = product_page.get_products_info()
+print(product_name)
+print(product_rating)
 
 
-product_page = ProductReader(url="https://shopee.com.my/product/26912497/1881778163", driver=driver)
-soup = product_page.soup
 
-print(soup)
+
 
 
 
--- product_reader.py
+++ product_reader.py
@@ -1,8 +1,5 @@
-# import libraries required for the reader class
-import requests
-# import beautiful soup library
-from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 class ProductReader:
@@ -15,7 +12,39 @@ class ProductReader:
         Initialize the class with the url to get website data
         """
         self.url = url
-        driver.get(self.url)
-        self.html = driver.page_source
-        self.soup = BeautifulSoup(self.html, "html.parser")
+        self.driver = driver
+        self.driver.get(self.url)
+        self.html = self.driver.page_source
+
+    def get_products_info(self):
+        """
+        parse the product information from the soup text attribute
+
+        :return:
+        product name
+        product rating
+        """
+
+        # get product name using css selector
+        product_name = self.driver.find_element_by_css_selector(".product-briefing .flex span").text
+
+        # get product rating using xpath
+        product_xpath = self.driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[2]/div/div[2]/div['
+                                                          '3]/div/div[2]/div[1]/div[1]').text
+
+        return product_name, product_xpath
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
